561a1f2ac92f5d9990ead5ed53ea061bdd379050
David Ger <draker74@gmail.com>
2019-09-26 19:08:02 +0300

    isolution react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <div className="container">
+        <div className="row">
+            <div className="col-md-6">David</div>
+            <div className="col-md-6">Ger</div>
+
+        </div>
+      </div>
+  );
+}
+
+export default App;
