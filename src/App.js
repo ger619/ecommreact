f7c6a64b5bf8e605269ad7301fca11c3d6183805
David Ger <draker74@gmail.com>
2019-09-27 19:48:46 +0300

    Isolution Upto Products

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-      <div className="container">
-        <div className="row">
-            <div className="col-md-6">David</div>
-            <div className="col-md-6">Ger</div>
-
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar />
+          <Switch>
+            <Route exact path="/" component={ProductList} /> 
+            <Route path="/details" component={Details} /> 
+            <Route path="/cart" component={Cart} /> 
+            <Route component={Default} /> 
+          </Switch>
+      </React.Fragment>
   );
 }
 
