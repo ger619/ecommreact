8cfd2e04987277649197ab55d4a4d4065cf6ef5c
David Ger <draker74@gmail.com>
2019-09-29 20:18:02 +0300

    Final push "29th Septermber 2019"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,12 @@ export default class Navbar extends Component {
                <ul className="navbar-nav align-items-center">
                    <li className="nav-item ml-5">
                        <Link to="/" className="nav-link">
-                           products
+                           goods 
+                       </Link>
+                   </li>
+                   <li className="nav-item ml-5">
+                       <Link to="/" className="nav-link">
+                           services
                        </Link>
                    </li>
                </ul>
--- src/context.js
+++ src/context.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
 import  {storeProducts, detailProduct} from './data'
-
-
 const ProductContext = React.createContext();
 
 
 class ProductProvider extends Component {
-    state ={
-        products:storeProducts,
-        detailProduct:detailProduct
+    state = {
+      products: [],
+      detailProduct: detailProduct,
+      cart: [],
+      modalOpen: false,
+      modalProduct: detailProduct,
+      cartSubTotal: 0,
+      cartTax: 0,
+      cartTotal: 0
     };
-    handleDetail =() =>{
-           console.log('hello Deatials');
-       };
+    componentDidMount() {
+      this.setProducts();
+    }
+  
+    setProducts = () => {
+      let products = [];
+      storeProducts.forEach(item => {
+        const singleItem = { ...item };
+        products = [...products, singleItem];
+      });
+      this.setState(() => {
+        return { products };
+      }, this.checkCartItems);
+    };
+      
+    getItem = id => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
+      };
+
+    handleDetail = id =>{
+        const product = this.getItem(id);
+        this.setState(() => {
+          return { detailProduct: product };
+        });
+      };
+
    addToCart =() =>{
            console.log('hello from Cart');
        };
