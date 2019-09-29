3844a424d9a06bb2543873109457d91a227b3d14
David Ger <draker74@gmail.com>
2019-09-29 16:40:35 +0300

    product complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,6 @@ export default class Navbar extends Component {
                     <span className="mr-2">
                         <i className="fas fa-cart-plus" />
                     </span>
-                    
                     My Cart
                 </ButtonContainer>
                </Link>
